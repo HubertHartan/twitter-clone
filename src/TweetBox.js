14ed8e3c5ae7f4dc9bb13690fc6e17e0986e022a
Hubert Hartan <hubertyhartan@gmail.com>
2023-01-01 20:20:41 +1100

    tweetbox continued

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import "./TweetBox.css";
-import {Button} from '@mui/material';
+import {Avatar, Button} from '@mui/material';
 
 
 function TweetBox() {
@@ -9,7 +9,9 @@ function TweetBox() {
     <div className='tweetBox'>
         <form>
             <div className='tweetBox__input'>
-
+                <Avatar src="https://upload.wikimedia.org/wikipedia/commons/1/11/Canis_lupus_familiaris.002_-_Monfero.jpg"/>
+                {/* Credits to https://upload.wikimedia.org/wikipedia/commons/1/11/Canis_lupus_familiaris.002_-_Monfero.jpg for the hardcoded profile */}
+                <input placeholder="What's happening?" type="text"/>
             </div>
 
             <Button>
