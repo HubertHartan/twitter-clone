ee3b04a92583e56c162982b462be1b89f7ec4dd1
Hubert Hartan <hubertyhartan@gmail.com>
2023-01-06 23:18:51 +1100

    continued work on post component

--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Post.css';
 import {Avatar} from '@mui/material';
 import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+import { ImageSharp } from '@mui/icons-material';
 
 function Post({
     avatar,
@@ -22,12 +23,20 @@ function Post({
           <div className='post__header'>
             <div className='post__headerText'>
               <h3>
-                Hubert Hartan <span>
-                  <VerifiedUserIcon className='post__badge'/>
+                Hubert Hartan {" "}
+                <span>
+                  <VerifiedUserIcon className='post__badge'/> @huberthartan
                 </span>
               </h3>
             </div>
+
+            <div className='post__headerDesc'>
+              <p>
+                I'm challenging myself to build a twitter clone.
+              </p>
+            </div>
           </div>
+          <img src="https://media2.giphy.com/media/iJDJbzZcsLvCXm7RXf/giphy.gif?cid=ecf05e47592bd9f5c65ad38086d4125738953e9c833d4f1a&rid=giphy.gif&ct=s" alt="placeholder"/>
         </div>
 
     </div>
