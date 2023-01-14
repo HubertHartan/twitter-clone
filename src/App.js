227f820bc5cac6c227a23c3515dd751b810a0cf9
Hubert Hartan <hubertyhartan@gmail.com>
2023-01-14 23:45:27 +1100

    imported widgets to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Widgets from './Widgets';
+
+
 
 function App() {
   return (
@@ -13,7 +16,7 @@ function App() {
       <Feed/>
 
       {/*Widgets*/}
-
+      <Widgets/>
 
     </div>
   );
