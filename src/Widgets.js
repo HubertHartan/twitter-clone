5da48c339ef0c66e99cd01d32252dcb1253440a7
Hubert Hartan <hubertyhartan@gmail.com>
2023-01-19 23:40:34 +1100

    added icons and input

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import 'Widgets.css';
+import './Widgets.css';
 import {
   TwitterTimelineEmbed,
   TwitterShareButton,
   TwitterTweetEmbed,
 } from "react-twitter-embed";
-
+import SearchIcon from '@mui/icons-material/Search';
 
 function Widgets() {
   return (
     <div className='widgets'>
       <div className="widgets__input">
-        <h2>Widgets</h2>
+        <SearchIcon className="widgets__searchIcon"/>
+        <input placeholder="Search Twitter" type="text"/>
       </div>
 
     </div>
