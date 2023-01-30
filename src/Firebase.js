6e3b4e09fd5f09ece22d0ef3057c22d16b320477
Hubert Hartan <hubertyhartan@gmail.com>
2023-01-30 23:14:14 +1100

    started backend

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAhZJXTU8A7ETh2wau52JlMdnIw-vGziDY",
+    authDomain: "twitter-clone-bad91.firebaseapp.com",
+    projectId: "twitter-clone-bad91",
+    storageBucket: "twitter-clone-bad91.appspot.com",
+    messagingSenderId: "986503674549",
+    appId: "1:986503674549:web:ba76db25d7bfa1e8295cfc",
+    measurementId: "G-997KXN30M9"
+  };
